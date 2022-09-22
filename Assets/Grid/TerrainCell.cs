a33e40fecf5ffcbbbe184cd91dce453dfba59eba
Christie Partington <christiedpartington@gmail.com>
2022-09-21 17:12:43 -0700

    Fix terrain type overlaps

--- Assets/Grid/TerrainCell.cs
+++ Assets/Grid/TerrainCell.cs
@@ -15,20 +15,22 @@ public class TerrainCell
         this.x = x;
         this.z = z;
         
-        float[] LevelPercentiles = TerrainComputer.Instance.TerrainLevelPercentiles;
-        for (int i = 0; i < LevelPercentiles.Length; i++)
+        float[] levelPercentiles = TerrainComputer.Instance.TerrainLevelPercentiles;
+        for (int i = 0; i < levelPercentiles.Length; i++)
         {
-            if (noiseValue < LevelPercentiles[i])
+            if (noiseValue < levelPercentiles[i])
             {
                 this.y = i;
                 break;
             }
         }
 
-        float[] TypePercentiles = TerrainComputer.Instance.TerrainTypePercentiles;
-        for (int i = 0; i < TypePercentiles.Length; i++)
+        float[] typePercentiles = TerrainComputer.Instance.TerrainTypePercentiles;
+        float percentile = this.y / (float)(TerrainComputer.Instance.TerrainLevelPercentiles.Length - 1);
+        Debug.Log(percentile + " --- " + typePercentiles.ToString());
+        for (int i = 0; i < typePercentiles.Length; i++)
         {
-            if (noiseValue < TypePercentiles[i])
+            if (percentile < typePercentiles[i])
             {
                 this.type = (TerrainType)i;
                 break;
